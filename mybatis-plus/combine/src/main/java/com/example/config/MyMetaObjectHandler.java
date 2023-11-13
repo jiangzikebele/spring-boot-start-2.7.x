7cbbcc1aa78f2b3cd40ffc15104a2a052a5be7ee
某人 <17620007371@163.com>
2023-11-13 16:19:27 +0800

    0.5.2 添加配置

---
+++ mybatis-plus/combine/src/main/java/com/example/config/MyMetaObjectHandler.java
@@ -0,0 +1,24 @@
+package com.example.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        // 在插入时自动填充创建时间和更新时间字段
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        // 在更新时自动填充更新时间字段
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
